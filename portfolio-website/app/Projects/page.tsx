bf5bead88b279d2b84ceef473e49b36e206af212
robertojuarezjose <134658833+robertojuarezjose@users.noreply.github.com>
2025-08-03 14:55:06 -0500

    Made an update to my resume

--- portfolio-website/app/Projects/page.tsx
+++ portfolio-website/app/Projects/page.tsx
@@ -10,34 +10,67 @@ const projects = [
         "Name": "Verification Cargo System",
         "Description": "Verifies cargo goes out with the correct truck and driver.",
         "Url": "",
-        "Date_Created": "2024-11-27",
-        "Tech_Stack": ["Next.js", "TypeScript", "Tailwind CSS", "Supabase", "prisma"],
-        "Owner": "Jose Roberto Juarez"
+        "Date_Created": "2023-09-27",
+        "Owner": "Rocket Labs Technologies / Buckland "
     },
     {
         "Name": "Store ecommerce and inventory management system",
         "Description": "A real-time reports and inventory management system.",
         "Url": "",
-        "Date_Created": "2023-08-13",
-        "Tech_Stack": ["Next.js", "TypeScript", "Tailwind CSS", "Supabase", "prisma"],
-        "Owner": "Jose Roberto Juarez"
+        "Date_Created": "2023-02-13",
+        "Owner": "Rocket Labs Technologies"
     },
     {
-        "Name": "Warehouse Management System",
+        "Name": "Slam Distribution",
         "Description": "Slam distribution warehouse management system.",
         "Url": "",
-        "Date_Created": "2024-06-14",
-        "Tech_Stack": ["Jquery", "C# .Net", "Sql Server"],
-        "Owner": "Op Computer business Solutions"
+        "Date_Created": "2024-07-14",
+        "Owner": "Op Computer business Solutions / Shared System"
     },
     {
         "Name": "Single Sign on and Authentication",
         "Description": "Single sign on and authentication for the application suite for Heb",
         "Url": "",
         "Date_Created": "2022-11-20",
-        "Tech_Stack": ["React.js", "Flask"],
         "Owner": "HEB Digital"
     },
+    {
+        "Name": "First Cash",
+        "Description": "A solution to detect locking technology for First Cash.",
+        "Url": "",
+        "Date_Created": "2023-05-22",
+        "Owner": "Rocket Labs Technologies"
+    },
+    {
+        "Name": "Airpot Monitor",
+        "Description": "Airpot like monitor for real-time Dashboards displaying the live status of cargo boxes",
+        "Url": "https://medium.com/@robertojuarezjose/real-time-dasboard-displaying-the-live-status-of-cargo-boxes-from-warehouse-arrival-through-13cc2893cb13",
+        "Date_Created": "2025-07-24",
+        "Owner": "Op Computer business Solutions / SR Fowarding / Laredo Cold Services",
+    },
+    {
+        "Name": "Logistics Management System & monitor",
+        "Description": "This is a logistics system thats connected to a traffic and order system. It helps monitor incoming and outgoing cargo, upload/download documents, and prepare traffic data to be set before arrival. ",
+        "Url": "",
+        "Date_Created": "2023-05-22",
+        "Owner": "Op Computer business Solutions / RXO Logistics"
+    },
+    {
+        "Name": "Monitor de Colores",
+        "Description": "this monitor helps visual the times of each step in the process of cargo management.",
+        "Url": "",
+        "Date_Created": "2023-05-22",
+        "Owner": "Op Computer business Solutions / SR Fowarding / Laredo Cold Services"
+    },
+    {
+        "Name": "Monitor de Trazabilidad",
+        "Description": "this monitor helps visual the time of each traffic reference and the required documents that have been uploaded for each reference. The table has interactive buttons to upload documents and mark events of each traffic reference.",
+        "Url": "",
+        "Date_Created": "2025-03-03",
+        "Owner": "Op Computer business Solutions / American Export"
+    },
+    
+    
 
 ];
 
@@ -61,7 +94,6 @@ const Page = () => {
                     <TableRow>
                         <TableHead>Name</TableHead>
                         <TableHead>Description</TableHead>
-                        <TableHead>Tech Stack</TableHead>
                         <TableHead>Date Created</TableHead>
                         <TableHead>Owner</TableHead>
                         {/*<TableHead>Link</TableHead>*/}
@@ -72,7 +104,6 @@ const Page = () => {
                         <TableRow key={index}>
                             <TableCell>{project.Name}</TableCell>
                             <TableCell>{project.Description}</TableCell>
-                            <TableCell>{project.Tech_Stack.join(", ")}</TableCell>
                             <TableCell>{project.Date_Created}</TableCell>
                             <TableCell>{project.Owner}</TableCell>
                             {/*<TableCell>*/}
